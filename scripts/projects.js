e30d87cda5e74b07fd5f411b0c42506c42c12b7f
Justas <zdanaviciusjustas1@gmail.com>
2021-11-19 23:54:39 +0100

    Animation progress

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,118 +1,132 @@
 const wrapper = document.querySelector(".project-wrapper");
 const cta = document.querySelector(".cta");
-const centerX = wrapper.offsetWidth/4*3;
+const centerX = wrapper.offsetWidth/2;
 const centerY = wrapper.offsetHeight/2;
 
-const smile = [95, 46, 56, 62, 68, 74, 55, 36, 17];
-
 const projectCount = 1000;
-const maxFollow = 250;
-const rotationIntensity = 20;
-let a = 7;
+const maxFollow = 850;
+const rotationIntensity = 50;
+let projectElements;
+let a = 12;
 let rotation = -1000;
 let isOpen = false;
 let nX = 0;
 let nY = 0;
 let mX = 0;
 let mY = 0;
+let inPosition = true;
 
-function renderProjects() {
-    while(wrapper.firstChild){
-        wrapper.removeChild(wrapper.firstChild);
-    }
-    projects = [];
+let wrapperWidth = wrapper.offsetWidth;
+let wrapperHeight = wrapper.offsetHeight;
 
-    for(let index = 0; index < projectCount; index++){
-        let projectElement = document.createElement("div");
-        projectElement.classList.add("project");
-
-        let angle = index; // + (rotation * Math.PI / 180);
-        let dist = a * Math.sqrt(angle+rotation);
-        // dist = Math.cos(dist)*100;
-
-        let xCart = dist*Math.cos(angle);
-        let yCart = dist*Math.sin(angle);
-
-        projectElement.style.top = (centerY - yCart) + "px";
-        projectElement.style.left = (centerX - xCart) + "px";
-        projectElement.style.opacity = (1 - (index/projectCount));
-        projectElement.style.filter = "hue-rotate(" + index/6 + "deg)";
-        projectElement.style.zIndex = 1000-index;
-        // projectElement.style.transform = "rotate(" + Math.asin(xCart/yCart)*180/Math.PI + "rad)"
-
-        // if(smile.includes(index)){
-        //     projectElement.style.backgroundColor = "black";
-        // }
-
-        // if(index < 150){
-            // projectElement.style.backgroundColor = "blue";
-        // }
-
-        let project = {
-            top: (centerY - yCart),
-            left: (centerX - xCart)
-        }
-
-        wrapper.appendChild(projectElement);
-        projects.push(project);
-    }
-
-    updateProjectPosition();
-
-}
-
-function updateProjectPosition(){
-    const pcX = centerX + maxFollow*nX/centerX;
-    const pcY = centerY + maxFollow*nY/centerY;
-
-    // const spotter = document.querySelector("#spotter");
-    // spotter.style.top = pcY + "px";
-    // spotter.style.left = pcX + "px";
-    // spotter.style.zIndex = "2000";
-
-    cta.style.top = pcY - 50 + "px";
-    cta.style.left = pcX - 50 + "px";
-
-    let distanceFromCenter = Math.sqrt(Math.pow(mX - pcX, 2) + Math.pow(mY - pcY, 2));
-
-    if(distanceFromCenter < 100 && !isOpen){
-        openProject();
-    }
-    else if (distanceFromCenter >= 100 && isOpen){
-        closeProject();
-    }
-
-    document.querySelectorAll(".project").forEach((project, index) => {
-        const oldX = projects[index].left;
-        const oldY = projects[index].top;
-        const closeness = 1 - (index/projectCount*1);
-
-        const newX = +oldX + (+maxFollow * (nX*(1 - index/projectCount)*2) * closeness / (centerX*2));
-        const newY = +oldY + (+maxFollow * (nY*(1 - index/projectCount)*2) * closeness / (centerY*2));
-
-        project.style.left = newX + "px";
-        project.style.top = newY + "px";
-    });
-}
-
+//INITIAL SETUP
 function initProjectViewer(){
-    renderProjects();
     loadProject();
 
     cta.addEventListener("click", (e) => {
         unloadProject();
     })
 
-    window.addEventListener("mousemove", (e) => {
-        nX = e.clientX - centerX;
-        nY = e.clientY - centerY;
-        mX = e.clientX;
-        mY = e.clientY;
+    // window.addEventListener("mousemove", (e) => {
+    //     nX = e.clientX - centerX;
+    //     nY = e.clientY - centerY;
+    //     mX = e.clientX;
+    //     mY = e.clientY;
 
-        updateProjectPosition();
+    //     updateProjectPosition();
+    // });
+    updateProjectPosition();
+}
+
+//GENERATE ELEMENTS
+function generateProjects(){
+    //Remove all child elements
+    while(wrapper.firstChild){
+        wrapper.removeChild(wrapper.firstChild);
+    }
+    projects = [];
+
+    //Create new elements
+    for(let index = 0; index < projectCount; index++){
+        const projectElement = document.createElement("div");
+        projectElement.classList.add("project");
+
+        const project = {top: 0, left: 0 };
+
+        //Custom styling based on index
+        projectElement.style.opacity = (1 - (index/projectCount));
+        projectElement.style.zIndex = 1000-index;
+
+        //Add to DOM
+        wrapper.appendChild(projectElement);
+        projects.push(project);
+    }
+
+    projectElements = wrapper.querySelectorAll(".project");
+}
+
+//SET PROJECTS POSITION
+function renderProjects() {
+    for(let index = 0; index < projects.length; index++){
+        const angle = index;
+        const distMultiplier = 0.5*Math.tan(index/projectCount*Math.PI/3) + 1;
+        const dist = a * Math.sqrt(angle+rotation) * distMultiplier;
+
+        projects[index] = {
+            //Turn poral coordinates to cartesian
+            top: centerY - dist*Math.sin(angle),
+            left: centerX - dist*Math.cos(angle)
+        }
+    }
+
+    updateProjectPosition();
+}
+
+//INTRO ANIMATION
+function loadProject(){
+    rotation = -1200;
+    generateProjects();
+    const interval = setInterval(() => {
+        rotation += 10;
+        if(rotation >= 0){
+            clearInterval(interval);
+            rotation = 0;
+        }
+        renderProjects();
+    }, 1);
+}
+
+//UPDATE POSITION
+function updateProjectPosition(){
+    const pcX = centerX + maxFollow*nX/wrapperWidth;
+    const pcY = centerY + maxFollow*nY/wrapperHeight;
+
+    cta.style.top = pcY - 50 + "px";
+    cta.style.left = pcX - 50 + "px";
+
+    const distanceFromCenter = Math.sqrt(Math.pow(mX - pcX, 10) + Math.pow(mY - pcY, 2));
+
+    // if(distanceFromCenter < 40 && !isOpen){
+    //     openProject();
+    // }
+    // else if (distanceFromCenter >= 40 && isOpen){
+    //     closeProject();
+    // }
+
+    projectElements.forEach((project, index) => {
+        const orgX = projects[index].left;
+        const orgY = projects[index].top;
+        const closeness = 1 - index/projectCount;
+
+        const diffX = maxFollow*nX*Math.pow(closeness, 2)/wrapperWidth;
+        const diffY = maxFollow*nY*Math.pow(closeness, 2)/wrapperHeight;
+
+        project.style.left = orgX + diffX + "px";
+        project.style.top = orgY + diffY + "px";
     });
 }
 
+//OPEN CTA
 function openProject(){
     const openingInterval = setInterval(() => {
         rotation += rotationIntensity;
@@ -123,9 +137,10 @@ function openProject(){
             isOpen = true;
         }
         renderProjects();
-    }, 10);
+    }, 1);
 }
 
+//CLOSE CTA
 function closeProject(){
     cta.style.opacity = 0;
     const closingInterval = setInterval(() => {
@@ -136,21 +151,10 @@ function closeProject(){
             isOpen = false;
         }
         renderProjects();
-    }, 10);
-}
-
-function loadProject(){
-    rotation = -1000;
-    const interval = setInterval(() => {
-        rotation += rotationIntensity;
-        if(rotation >= 0){
-            clearInterval(interval);
-            rotation = 0;
-        }
-        renderProjects();
     }, 1);
 }
 
+//OUTRO ANIMATION
 function unloadProject(){
     const interval = setInterval(() => {
         rotation += rotationIntensity;
@@ -163,11 +167,26 @@ function unloadProject(){
             window.location = "/#/about";
         }
         renderProjects();
-    }, 10);
+    }, 1);
+}
+
+function generateNewPosition(){
+    const maxStep = 20;
+    const pcX = centerX + maxFollow*nX/wrapperWidth;
+    const pcY = centerY + maxFollow*nY/wrapperHeight;
+
+    const randomX = Math.random()*maxStep*2 - maxStep;
+    const randomY = Math.random()*maxStep*2 - maxStep;
+    nX += randomX;
+    nY += randomY;
+
+    updateProjectPosition();
+    // sleep(1000);
+    // generateNewPosition();
 }
 
 function sleep(ms) {
-return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 initProjectViewer();
